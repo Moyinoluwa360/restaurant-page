5ab63ef5a92a163165d4b1751a428022045caaac
Moyinoluwa360 <ogundanajoseph17@gmail.com>
2024-08-03 00:32:38 +0100

    created all pages no logic is wrtten yet

--- src/about.js
+++ src/about.js
@@ -1,16 +1,12 @@
-import "./style.css"
-import jollof from "./jollof.jpg"
-import menuComponent from "./menu.js"
-import aboutComponent from "./about.js"
-function homeComponents(){
-    const contentBody = document.createElement("div")
-    contentBody.setAttribute('id',"contentBody")
+export default function aboutComponent(){
+    const aboutBody = document.createElement("div")
+    aboutBody.setAttribute('id',"contentBody")
 
     const header = document.createElement("h3")
-    const welcomeNote = document.createElement("div")
+    const aboutNote = document.createElement("div")
     //
-    header.textContent = "Welcome To Kukas"
-    welcomeNote.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, veritatis corrupti eum cupiditate debitis provident similique illo voluptatibus! Explicabo temporibus ea nobis numquam exercitationem? Fuga nobis nemo magnam. Similique, accusantium." + 
+    header.textContent = "About Us"
+    aboutNote.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, veritatis corrupti eum cupiditate debitis provident similique illo voluptatibus! Explicabo temporibus ea nobis numquam exercitationem? Fuga nobis nemo magnam. Similique, accusantium." + 
                                 "\n" + "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto excepturi consectetur earum omnis sint expedita harum dicta est. Saepe natus eveniet sint id consequatur harum, facilis a voluptate optio?" +
                                 "\n" + "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto excepturi consectetur earum omnis sint expedita harum dicta est. Saepe natus eveniet sint id consequatur harum, facilis a voluptate optio?" + 
                                 "\n" + "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto excepturi consectetur earum omnis sint expedita harum dicta est. Saepe natus eveniet sint id consequatur harum, facilis a voluptate optio?" +
@@ -20,10 +16,7 @@ function homeComponents(){
                                 "\n" + "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto excepturi consectetur earum omnis sint expedita harum dicta est. Saepe natus eveniet sint id consequatur harum, facilis a voluptate optio?" +
                                 "\n" + "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto excepturi consectetur earum omnis sint expedita harum dicta est. Saepe natus eveniet sint id consequatur harum, facilis a voluptate optio?" 
     //
-    contentBody.appendChild(header)
-    contentBody.appendChild(welcomeNote)
-    return contentBody
+    aboutBody.appendChild(header)
+    aboutBody.appendChild(aboutNote)
+    return aboutBody
 }
-
-const content = document.querySelector("#content")
-content.appendChild(aboutComponent())
